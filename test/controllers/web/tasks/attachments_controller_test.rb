e11c4401ee4136386c67279e0497e47ecc4d9eaa
Antonov Grigory <antonovga@gmail.com>
2016-10-03 17:53:46 +0300

    forgot to add attachments controller test

---
+++ test/controllers/web/tasks/attachments_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class Web::Tasks::AttachmentsControllerTest < ActionDispatch::IntegrationTest
+  test 'should download attachcment file' do
+    @task = tasks(:regular_user_task)
+
+    File.open('test/fixtures/files/example.pdf') do |f|
+      @task.attachment = f
+    end
+
+    @task.save
+
+    sign_in
+    get download_task_attachment_path(@task)
+
+    assert_response :success
+    assert_equal MIME::Types.type_for(@task.attachment.file.filename).first&.to_s || 'application/octet-stream',
+                 response.content_type
+  end
+end
